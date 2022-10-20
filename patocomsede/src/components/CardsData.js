cf52c41b399a19c834869727c3c5e4600501af22
thiagohenrique23 <thiagohenrique2309@gmail.com>
2022-10-20 11:37:21 -0400

    Criação de componentes

---
+++ patocomsede/src/components/CardsData.js
@@ -0,0 +1,34 @@
+const Cardsdata = [
+    {
+        id: 1,
+        rname: "",
+        imgdata: "",
+        delimg: "",
+        price: 1000,
+        rating: "10",
+        arrimg: "",
+        qnty: 0
+    },
+
+    {
+        id: 2,
+        rname: "",
+        imgdata: "",
+        delimg: "",
+        price: 1000,
+        rating: "10",
+        arrimg: "",
+        qnty: 0
+    },
+    
+    {
+        id: 3,
+        rname: "",
+        imgdata: "",
+        delimg: "",
+        price: 1000,
+        rating: "10",
+        arrimg: "",
+        qnty: 0
+    }
+]
