938e4221ed7b3a5047732eaef980704db0394e94
thiagohenrique23 <thiagohenrique2309@gmail.com>
2022-10-24 19:44:12 -0400

    100%

--- patocomsede/src/components/CardsData.js
+++ patocomsede/src/components/CardsData.js
@@ -2,36 +2,69 @@ const Cardsdata = [
     {
         id: 1,
         rname: "Royal Salute",
-        imgdata: "https://m.media-amazon.com/images/I/71s+a3OLxpL._AC_SX679_.jpg",
-        delimg: "https://m.media-amazon.com/images/I/71s+a3OLxpL._AC_SX679_.jpg",
+        imgdata: "https://www.emporiofreicaneca.com.br/wp-content/uploads/2019/01/Whisky-Royal-Salute-21-anos-The-Signature-Blend-700ml.png",
+        delimg: "https://www.emporiofreicaneca.com.br/wp-content/uploads/2019/01/Whisky-Royal-Salute-21-anos-The-Signature-Blend-700ml.png",
         somedata: "777 + pedido feito recentemente",
         price: 1000,
         rating: "10",
-        arrimg: "https://m.media-amazon.com/images/I/71s+a3OLxpL._AC_SX679_.jpg",
+        arrimg: "https://www.emporiofreicaneca.com.br/wp-content/uploads/2019/01/Whisky-Royal-Salute-21-anos-The-Signature-Blend-700ml.png",
         qnty: 0
     },
 
     {
         id: 2,
         rname: "White Horse",
-        imgdata: "https://m.media-amazon.com/images/I/51WFhauibXS._AC_SX679_.jpg",
-        delimg: "https://m.media-amazon.com/images/I/51WFhauibXS._AC_SX679_.jpg",
+        imgdata: "https://www.emporiofreicaneca.com.br/wp-content/uploads/2020/05/Whisky-White-Horse-1l.png",
+        delimg: "https://www.emporiofreicaneca.com.br/wp-content/uploads/2020/05/Whisky-White-Horse-1l.png",
         somedata: "777 + pedido feito recentemente",
         price: 97,
         rating: "10",
-        arrimg: "https://m.media-amazon.com/images/I/51WFhauibXS._AC_SX679_.jpg",
+        arrimg: "https://www.emporiofreicaneca.com.br/wp-content/uploads/2020/05/Whisky-White-Horse-1l.png",
         qnty: 0
     },
     
     {
         id: 3,
         rname: "Jack Daniels",
-        imgdata: "https://m.media-amazon.com/images/I/61+8jWEM9gL._AC_SX679_.jpg",
-        delimg: "https://m.media-amazon.com/images/I/61+8jWEM9gL._AC_SX679_.jpg",
+        imgdata: "https://www.emporiofreicaneca.com.br/wp-content/uploads/2018/11/jack-daniels.png",
+        delimg: "https://www.emporiofreicaneca.com.br/wp-content/uploads/2018/11/jack-daniels.png",
         somedata: "777 + pedido feito recentemente",
         price: 179,
         rating: "8",
-        arrimg: "https://m.media-amazon.com/images/I/61+8jWEM9gL._AC_SX679_.jpg",
+        arrimg: "https://www.emporiofreicaneca.com.br/wp-content/uploads/2018/11/jack-daniels.png",
+        qnty: 0
+    },
+    {
+        id: 4,
+        rname: "Old Parr",
+        imgdata: "https://www.emporiofreicaneca.com.br/wp-content/uploads/2021/01/Whisky-Old-Parr-12-anos-750ml-1.png",
+        delimg: "https://www.emporiofreicaneca.com.br/wp-content/uploads/2021/01/Whisky-Old-Parr-12-anos-750ml-1.png",
+        somedata: "777 + pedido feito recentemente",
+        price: 99,
+        rating: "10",
+        arrimg: "https://www.emporiofreicaneca.com.br/wp-content/uploads/2021/01/Whisky-Old-Parr-12-anos-750ml-1.png",
+        qnty: 0
+    },
+    {
+        id: 5,
+        rname: "Buchanans",
+        imgdata: "https://www.emporiofreicaneca.com.br/wp-content/uploads/2019/08/buchanans-1.png",
+        delimg: "https://www.emporiofreicaneca.com.br/wp-content/uploads/2019/08/buchanans-1.png",
+        somedata: "777 + pedido feito recentemente",
+        price: 133,
+        rating: "10",
+        arrimg: "https://www.emporiofreicaneca.com.br/wp-content/uploads/2019/08/buchanans-1.png",
+        qnty: 0
+    },
+    {
+        id: 6,
+        rname: "Ballantines",
+        imgdata: "https://cdn.iset.io/assets/50100/produtos/2861/130135510sz.png",
+        delimg: "https://cdn.iset.io/assets/50100/produtos/2861/130135510sz.png",
+        somedata: "777 + pedido feito recentemente",
+        price: 99,
+        rating: "8",
+        arrimg: "https://cdn.iset.io/assets/50100/produtos/2861/130135510sz.png",
         qnty: 0
     }
 ]
